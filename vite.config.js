87df1c1e913e2732b453c0ad743ad561e989975a
Pasquale Ruotolo <pachiruo@gmail.com>
2026-03-19 18:42:30 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/elimsite/", // This ensures assets load correctly on GitHub Pages
+  base: "/elimsite/", // Essential for correct absolute asset paths on GH Pages
 });
